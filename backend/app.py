f446e3622e0bfa38884a6595d7dac72b8c12e59a
21C <twentyonec21c@gmail.com>
2025-03-23 01:13:43 +0530

    Separate utils

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,11 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from llm.llm import chat_messages, create_message
 import ollama
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
-system_prompt = "You are an assistant meant to help me fetch data and inform me about the query response. You receive data on vehicles (license plate, colour, type) that pass by a camera and is recorded in a video. I ask you questions about the vehicles that pass and your job is to answer them. Remember you do not have any data on you until i feed it to you. Do not hallucinate."
-
-chat_messages = [{'role': 'system', 'content': system_prompt}]
-
-def create_message(message, role):
-    return {
-        'role': role,
-        'content': message
-    }
 
 @app.route('/api/message', methods=['POST'])
 def handle_message():
