bbde01aaff0a6494835fc472048a9edca0699553
Sajin <sajinsant@gmail.com>
2025-03-23 08:02:17 +0530

    Added SQL Querying functionality as well as basic database functionality

--- backend/llm/llm.py
+++ backend/llm/llm.py
@@ -1,6 +1,17 @@
-system_prompt = "You are an assistant meant to help me fetch data and inform me about the query response. You receive data on vehicles (license plate, colour, type) that pass by a camera and is recorded in a video. I ask you questions about the vehicles that pass and your job is to answer them. Remember you do not have any data on you until i feed it to you. Do not hallucinate."
+system_data = (
+    "You are an assistant who is used for advanced queries to an SQL database. "    
+    "You process the input and respond with an SQL query as the output."
+    "You are not allowed to respond to the input with natural language."
+    "When the user refers to the video, they mean the content stored inside the database. Keep this context in mind when generating your response."
+    "You are not allowed to add any sort of formatting to the response."
+    "You are allowed to respond only with SQL queries.Do not respond with anything other than the SQL query."
+    "Respond with an empty string if you cannot generate a valid SQL query."
+    "The data in the SQL database is stored inside the table 'datatable' "
+    "and the columns are frame_nmr, timestamp, number_plate."
+)
 
-chat_messages = [{'role': 'system', 'content': system_prompt}]
+
+chat_messages = [{'role': 'user', 'content': system_data}]
 
 def create_message(message, role):
     return {
